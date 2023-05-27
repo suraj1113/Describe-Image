2886c7d52135152d70b53c0b4490360bdd9431f3
suraj1113 <62590327+suraj1113@users.noreply.github.com>
2023-05-27 15:40:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Speech from './components/Speech';
+import './App.css';
+const App = () => {
+  return (
+    <>
+      <h2>Describe About the Image</h2>
+       <Speech/>
+      
+    </>
+  )
+}
+
+export default App
